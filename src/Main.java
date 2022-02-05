dd712298506de947ab5c1fb468c7f5fd6d582f1e
zhouming <zm15638521439@163.com>
2022-02-05 14:12:23 +0800

    96. 不同的二叉搜索树

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,8 @@ public class Main {
 //        Subsets2 subsets2 = new Subsets2();
 //        System.out.println(subsets2.subsetsWithDup(new int[]{1,2,2}));
 //        System.out.println(StaticMethod.search2(new int[]{2,5,6,0,0,1,2},3));
-        MaximumGold maximumGold = new MaximumGold();
-        System.out.println(maximumGold.getMaximumGold(new int[][]{{0,6,0},{5,8,7},{0,9,0}}));
+//        MaximumGold maximumGold = new MaximumGold();
+//        System.out.println(maximumGold.getMaximumGold(new int[][]{{0,6,0},{5,8,7},{0,9,0}}));
 //        System.out.println(maximumGold.getMaximumGold(new int[][]{
 //                {1, 0, 7},
 //                {2, 0, 6},
@@ -26,5 +26,6 @@ public class Main {
 //                {0, 3, 0},
 //                {9, 0, 20}
 //        }));
+        System.out.println(StaticMethod.numTrees(4));
     }
 }
--- src/algorithm/StaticMethod.java
+++ src/algorithm/StaticMethod.java
@@ -1224,4 +1224,24 @@ public class StaticMethod {
         }
         return false;
     }
+
+    /**
+     * 96. 不同的二叉搜索树
+     * 动态规划
+     * dp[n] = dp[0]*dp[n-1] + dp[1]*dp[n-2] + ... + dp[i]*dp[n-i] + ... + dp[n-1]*dp[0]
+     * @param n
+     * @return
+     */
+    public static int numTrees(int n) {
+        // dp[n]表示n个结点的树种类有多少个
+        int[] dp = new int[n+1];
+        dp[0] = 1;// 空数种类只有一
+        dp[1] = 1;// 只有一个根节点的树种类只有一
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
 }
