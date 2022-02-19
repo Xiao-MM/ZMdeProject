ea1d22ff624bc52977ba86bce3110026fed7e311
zhouming <zm15638521439@163.com>
2022-02-19 09:59:32 +0800

    74. 搜索二维矩阵

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(StaticMethod.canJump(new int[]{0}));
-//        System.out.println(StaticMethod.findMinFibonacciNumbers(19));
-
-//        Subsets2 subsets2 = new Subsets2();
-//        System.out.println(subsets2.subsetsWithDup(new int[]{1,2,2}));
-//        System.out.println(StaticMethod.search2(new int[]{2,5,6,0,0,1,2},3));
-//        MaximumGold maximumGold = new MaximumGold();
-//        System.out.println(maximumGold.getMaximumGold(new int[][]{{0,6,0},{5,8,7},{0,9,0}}));
-//        System.out.println(maximumGold.getMaximumGold(new int[][]{
-//                {1, 0, 7},
-//                {2, 0, 6},
-//                {3, 4, 5},
-//                {0, 3, 0},
-//                {9, 0, 20}
-//        }));
-//        System.out.println(StaticMethod.numTrees(4));
-//        System.out.println(StaticMethod.removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}));
-//        System.out.println(StaticMethod.longestDiverseString(2, 2, 7));
-//        int[] nums = new int[]{2,1,4,3};
-//        System.out.println(Arrays.toString(nums));
-//        StaticMethod.nextPermutation(nums);
-//        System.out.println(Arrays.toString(nums));
-        System.out.println(StaticMethod.singleNonDuplicate(new int[]{3,3,7,7,10,11,11}));
+//        System.out.println(StaticMethod.searchMatrix(new int[][]{
+//                {1, 3, 5, 7},
+//                {10, 11, 16, 20},
+//                {23, 30, 34, 60}
+//        }, 13));
+        System.out.println(StaticMethod.searchMatrix(new int[][]{
+                {1,1}
+        }, 2));
     }
 }
--- src/algorithm/StaticMethod.java
+++ src/algorithm/StaticMethod.java
@@ -605,7 +605,7 @@ public class StaticMethod {
      * @param target
      * @return
      */
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix2(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
         for (int i = 0; i < m; i++) {
             int l = 0, r = n - 1;
@@ -1402,5 +1402,29 @@ public class StaticMethod {
 //        }
 //        return nums[r];
 //    }
+
+    /**
+     * 74. 搜索二维矩阵
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int l = 0, r = m * n - 1;
+        int mid;
+        while (l <= r){
+            mid = l + (r - l) / 2;
+            int val = matrix[mid / n][mid % n];
+            if (val == target){
+                return true;
+            }else if (val > target){
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
 
 }
