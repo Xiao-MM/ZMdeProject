b2ff21840feb3f453ecd7d4ac2572fd58c02efec
zhouming <zm15638521439@163.com>
2022-02-21 10:46:11 +0800

    63. 不同路径 II

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(StaticMethod.kthSmallest(new int[][]{
-                {1,5,9},
-                {10,11,13},
-                {12,13,15}
-        },8));
+//        System.out.println(StaticMethod.kthSmallest(new int[][]{
+//                {1,5,9},
+//                {10,11,13},
+//                {12,13,15}
+//        },8));
+        System.out.println(StaticMethod.uniquePathsWithObstacles(new int[][]{
+//                {0, 0, 0},
+//                {0, 1, 0},
+//                {0, 0, 0}
+//                {0,1},{0,0}
+                {1,0}
+        }));
+
     }
 }
